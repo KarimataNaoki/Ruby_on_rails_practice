1566e323f859ee67a64aa3336cf1b711648a9a15
KarimataNaoki <kn0109azay@gmail.com>
2022-11-23 00:40:39 +0900

    データベース作成

--- db/migrate/20221122152736_create_users.rb
+++ db/migrate/20221122152736_create_users.rb
@@ -1,11 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :name, limit:128, null:false
+      t.string :name
       t.integer :age
 
       t.timestamps
     end
-    add_index :users, name
   end
 end
